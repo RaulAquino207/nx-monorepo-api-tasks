7d8574d3d45b7b66c31773c42157d2ac45d716b9
Raul Aquino <aquinoraul207@gmail.com>
2024-03-17 15:38:30 -0300

    [main] added the two main applications and the ENV configuration

--- apps/tasks/src/app/app.module.ts
+++ apps/tasks/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import * as path from "path";
+
 @Module({
   imports: [ConfigModule.forRoot({
     ignoreEnvFile: process.env.NODE_ENV != 'development',
